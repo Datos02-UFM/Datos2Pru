069aeb519bd038d906aac51da29a3809609117b2
Mazinger <ricardojmv85@gmail.com>
2018-09-03 14:33:26 -0600

    Q

--- wikiAPI/app.js
+++ wikiAPI/app.js
@@ -149,6 +149,27 @@ app.get('/search/:topic/:userId', (req, res) => {
           connection.query(sql, function (err, result) {
           if (err) throw err;
             console.log("1 record inserted");
+            //Uso de redis para guardar 
+          
+         var redis = require('redis');
+         var client = redis.createClient();
+         
+         client.on('connect', function() {
+             console.log('Redis client connected');
+         });
+         
+         client.on('error', function (err) {
+             console.log('Something went wrong ' + err);
+         });
+         
+         client.set(myTopic, cleanArticles2, redis.print);
+         client.get(myTopic, function (error, result) {
+             if (error) {
+                 console.log(error);
+                 throw error;
+             }
+             console.log('GET result ->' + result);
+         });
           });
         });
     }else{
