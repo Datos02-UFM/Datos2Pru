c7a5c21396254aa086b156a76be151917516afec
Luis Angel <tortoladev@gmail.com>
2018-11-22 16:58:42 -0600

    API updated

--- logger.js
+++ logger.js
@@ -2,17 +2,16 @@ var request = require('request')
 const express = require('express')
 const app = express()
 var fs = require('fs');
-app.get('/log/:topic/:userId/:requestTimestamp', (req, res) => {
+app.get('/log/:topic/:userId/', (req, res) => {
     const topic = req.params.topic
     const userId = req.params.userId
-    const requestTimestamp = req.param.requestTimestamp
-    fs.appendFile('~/tmp/logstash.txt', userId+", "+topic + ", " + requestTimestamp +"\r\n", function(err){
+    fs.appendFile('~/tmp/logstash.txt', userId+", "+topic +"\r\n", function(err){
         if(err){
             console.log(err);
         }
     });
 });
 
-app.listen(3003, () => {
-    console.log("Server is up and listening on 3003...")
+app.listen(3000, () => {
+    console.log("Server is up and listening on 3000...")
   })
